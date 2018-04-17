d29f8303d15daa82f5c150dc0f7a727fda2ae09c
Zichen Wang <zichen.wang@mssm.edu>
2018-04-17 12:09:01 -0400

    Include secrete env var

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
+import os
 from bson.objectid import ObjectId
 # from pymongo import MongoClient
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
-MONGOURI = 'mongodb://127.0.0.1:27017/SCV'
+# MONGOURI='mongodb://146.203.54.131:27017/SCV'
+MONGOURI = os.environ.get('MONGOURI', 'mongodb://127.0.0.1:27017/SCV')
 # db = mongo.db
 
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 export ENTER_POINT='/embed'
+source secrets.sh
+echo $MONGOURI
+# export debug='0'
 python app.py
 unset ENTER_POINT
+unset MONGOURI
